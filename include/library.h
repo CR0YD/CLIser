f8c2feb9f0c8e438d27aecc463559678c48e909e
CR0YD <s.fiebranz0611@gmail.com>
2025-10-12 18:02:01 +0200

    :building_construction: Add project skeleton

---
+++ include/library.h
@@ -0,0 +1,7 @@
+//
+// Created by CR0YD on 10/12/25.
+//
+#ifndef CLISER_LIBRARY_H
+#define CLISER_LIBRARY_H
+
+#endif // CLISER_LIBRARY_H
---
+++ src/library.c
@@ -0,0 +1,4 @@
+//
+// Created by CR0YD on 10/12/25.
+//
+#include <library.h>
